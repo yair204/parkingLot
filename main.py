52c880accd1026326d120eebe4dbad62fc08dcb4
yair dagan~ <yairdagan2@gmail.com>
2023-02-05 15:04:31 +0200

    test

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     ticket_obj.set_csv_file()
     # ticket_obj.add_to_csvFile([1,2,3,4,5,2,3,4])
     parkinglot_obj.park_vehicle(ticket_obj,"corolla",123456,"red","S")
+    print("test")
     
     
     
