c10a947c48e07d5bf6fac9db0dc24e7d7e1a3793
yair dagan~ <yairdagan2@gmail.com>
2023-02-05 14:49:18 +0200

    wid

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+import parkingLot
+import ticket
+
+def main():
+    parkinglot_obj = parkingLot.AutomatedParkingLot(21) 
+    parkinglot_obj.allocate_n_slots()
+    ticket_obj = ticket.Ticket(1,2)
+    ticket_obj.set_csv_file()
+    # ticket_obj.add_to_csvFile([1,2,3,4,5,2,3,4])
+    parkinglot_obj.park_vehicle(ticket_obj,"corolla",123456,"red","S")
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
