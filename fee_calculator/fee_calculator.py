87e432ecd9eb8cb37c830ad825062d63967ce846
Ari ginsberger <arik150606@gmail.com>
2023-02-06 13:58:48 +0200

    fixed the calculate time

--- fee_calculator/fee_calculator.py
+++ fee_calculator/fee_calculator.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 import math
 
@@ -27,22 +28,12 @@ def calculate_time(Ticket: object) -> int:
     Returns:
         total_time:(int) final time the vehicle was in the parking lot, rounded up.
     """
-    entrance_time = Ticket.get_time()  # entry time
-    current_time = time.strftime("%H%M")  # departure time
-    current_date = time.strftime("%Y%m%d")  # today's date.
-    days = int(current_date[6:]) - int(Ticket.get_date()[6:])
-    minutes = (60 - int(entrance_time[3:])) + int(current_time[3:])
-    if days == 0:
-        hours = ((int(current_time[:2])) - (int(entrance_time[:2]))) * 60
-        return math.ceil(((hours + minutes) / 60))  # total time rounded up.
-    elif days > 0:
-        hours = (int(current_time[:2]) + (24 - int(entrance_time[:2]))) * 60
-        if days == 1:
-            return math.ceil(((hours + minutes) / 60))  # total time rounded up.
-        else:
-            return (
-                math.ceil(((hours + minutes) / 60)) + 24 * days - 1
-            )  # total time rounded up.
+    enter_time = Ticket.get_time()
+    exit_time = datetime.now()
+    different_time = exit_time - enter_time
+    total_seconds = different_time.total_seconds()
+    hours = total_seconds / 3600
+    return math.ceil(hours)  # total time round up
 
 
 def calculate_price(Ticket: object, price_of_days=480) -> int:
