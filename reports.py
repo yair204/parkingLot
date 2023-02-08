d10149acc38cd14835668665992fa8558332091e
yair dagan~ <yairdagan2@gmail.com>
2023-02-08 15:59:33 +0200

    add report

--- reports.py
+++ reports.py
@@ -3,6 +3,7 @@ import parkingLot
 import ticket
 import datetime
 import csv
+import fee_calculator
 
 def track_of_capacity(AutomatedParkingLot: object)->list:
     """_summary_
@@ -70,11 +71,15 @@ def specific_slot_ID(AutomatedParkingLot: object, plate_num: int)->str:
     slot_ID = (ticket.slot_ID for ticket in AutomatedParkingLot.tickets_list if ticket.plate_num == plate_num)
     return f"vehicle number: {plate_num} \n parking in slot number : {slot_ID}"  
 
-def vehicle_parked_in_period_time(fileName):
+def vehicle_parked_in_period_time(fileName:str) -> int:
     
     csv_list = list(csv.reader(open(fileName, 'r')))
     pay = csv_list[0].index('payment')
     return sum(int(col) for row in csv_list[1:] for col in row[pay:])
    
-        
+def parked_more_24_hours(AutomatedParkingLot: object) -> list[int]:
+    return [ticket.plate_num for ticket in AutomatedParkingLot.tickets_list if fee_calculator.calculate_time(ticket)> 24]    
+    
+    
+       
 print(vehicle_parked_in_period_time('data.csv'))
